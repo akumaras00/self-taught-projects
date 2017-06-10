ab465e5f258d6ef5efc516761cfabe5a8c74b296
Arul <java-interview@hotmail.com>
2017-06-10 10:41:45 +1000

    new println feature

--- simple-maven-project/src/main/java/com/mytutorial/HelloWorld.java
+++ simple-maven-project/src/main/java/com/mytutorial/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("My first java code from a Maven based Java project");
 		System.out.println("Changed in Git Hub");
+		System.out.println("Added a feature branch");
 	}
 }
