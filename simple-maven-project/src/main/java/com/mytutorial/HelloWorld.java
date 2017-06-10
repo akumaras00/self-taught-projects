52f2bed7b08213ebe7a49bd0c2aa8ce06d05cf9e
Arul <java-interview@hotmail.com>
2017-06-10 11:25:25 +1000

    user 1 initial commit

---
+++ simple-maven-project/src/main/java/com/mytutorial/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.mytutorial;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("My first java code from a Maven based Java project");
+		System.out.println("Changed in Git Hub");
+	}
+}
