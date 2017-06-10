c3cab26a331f632bac3913a2dd8a10bf96ffb0ad
Arul <java-interview@hotmail.com>
2017-06-10 11:12:40 +1000

    hh

--- simple-maven-project/src/main/java/com/mytutorial/HelloWorld.java
+++ simple-maven-project/src/main/java/com/mytutorial/HelloWorld.java
@@ -5,6 +5,5 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("My first java code from a Maven based Java project");
 		System.out.println("Changed in Git Hub");
-		System.out.println("Added a feature branch");
 	}
 }
